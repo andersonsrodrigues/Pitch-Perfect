334968e87636c15e7a18782162808144e963387a
andersonsrodrigues <srodrigues.anderson@gmail.com>
2019-10-04 10:25:15 +0100

    - Init

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startRecording(_ sender: Any) {
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
-
